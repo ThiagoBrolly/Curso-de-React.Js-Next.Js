04423bb4fd677f72c7fbdf7d7fcc309d80eba287
Thiago Brolly <thiagobrolly@THIAGOS-PAYLIVRE.localdomain>
2021-09-28 08:50:00 -0300

    Seção2 - 13.1.Componentes de classe com estado(stateful)

--- src/App.js
+++ src/App.js
@@ -3,34 +3,37 @@ import "./App.css";
 import { Component } from "react";
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.handlePClick = this.handlePClick.bind(this);
+  state = {
+    name: "John Doe",
+    counter: 0,
+  };
 
-    this.state = {
-      name: "John Doe",
-    };
-  }
-
-  handlePClick() {
+  handlePClick = () => {
     this.setState({ name: "Brolly" });
   }
 
+  handleAClick = (e) => {
+    e.preventDefault();
+    const { counter } = this.state;
+    this.setState({counter: counter + 1})
+  }
+
   render() {
-    const { name } = this.state;
+    const { name, counter } = this.state;
 
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p onClick={this.handlePClick}>{name}</p>
+          <p onClick={this.handlePClick}>{name} {counter}</p>
           <a
+            onClick={this.handleAClick}
             className="App-link"
             href="https://reactjs.org"
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            Link
           </a>
         </header>
       </div>
